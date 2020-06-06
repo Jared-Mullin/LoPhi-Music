9f0b9f39f02f37685369bfd7e9a9df355336b0e8
Jared <JaredMullin@ProtonMail.com>
2020-06-06 16:48:22 +0100

    Setup ESlint

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import VueRouter from 'vue-router';
-import Artists from './components/Artists';
-import Tracks from './components/Tracks';
-import Genres from './components/Genres';
-
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+import Artists from './components/Artists'
+import Tracks from './components/Tracks'
+import Genres from './components/Genres'
 
 Vue.config.productionTip = false
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 const routes = [
-  { path: '/tracks', component: Tracks, meta: {title: "Tracks"} },
-  { path: '/artists', component: Artists, meta: {title: "Artists"} },
-  { path: '/genres', component: Genres, meta: {title: "Genres"}},
+  { path: '/tracks', component: Tracks, meta: { title: 'Tracks' } },
+  { path: '/artists', component: Artists, meta: { title: 'Artists' } },
+  { path: '/genres', component: Genres, meta: { title: 'Genres' } }
 ]
 const router = new VueRouter({
   routes
@@ -23,4 +22,4 @@ new Vue({
   router,
   vuetify,
   render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
